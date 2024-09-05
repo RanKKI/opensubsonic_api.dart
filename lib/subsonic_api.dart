8ae760742258cca3e6d9479e42ccb617b419b04e
RanKKI <youloveonlymeh@gmail.com>
2024-09-06 08:17:02 +1000

    fix: export album info

--- lib/subsonic_api.dart
+++ lib/subsonic_api.dart
@@ -3,6 +3,7 @@ library subsonic_api;
 export 'src/apis/api.dart';
 export 'src/models/auth/auth.model.dart';
 export 'src/models/components/album/album_id3.model.dart';
+export 'src/models/components/album/album_info.model.dart';
 export 'src/models/components/artist/artist.model.dart';
 export 'src/models/components/artist/artist_id3.model.dart';
 export 'src/models/components/artist/artist_info.model.dart';
